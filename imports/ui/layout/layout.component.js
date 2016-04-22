ff480482692723044e6ebc4111dc6cba958403ed
Pankrashin Danil <danpan@yandex.ru>
2016-04-22 17:57:04 +0300

    sidenav

--- imports/ui/layout/layout.component.js
+++ imports/ui/layout/layout.component.js
@@ -7,9 +7,25 @@ import angularMeteor from 'angular-meteor';
 import './layout.view.html';
 
 class Layout {
-  constructor() {
+  constructor($mdSidenav) {
+    'ngInject';
+    this.$mdSidenav = $mdSidenav;
+    this.sideNavItems = [
+      {name:'Объекты', uisref:'realty'},
+      {name:'Клиенты', uisref:'realty'},
+      {name:'Дела', uisref:'realty'},
+      {name:'Отчет', uisref:'realty'},
+      {name:'Собщения', uisref:'realty'},
+      {name:'Документы', uisref:'realty'},
+      {name:'Настройки', uisref:'realty'},
+      {name:'Входящие', uisref:'realty'},
+      {name:'Исходящие', uisref:'realty'},
+      {name:'Модератор', uisref:'realty'}
+    ];
   }
-
+  toggleList() {
+    this.$mdSidenav('left').toggle();
+  }
 }
 
 const moduleName = 'layout';
