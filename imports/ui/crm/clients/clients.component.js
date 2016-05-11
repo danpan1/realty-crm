fa0c81ad942fbf720fa12928a52b99c8942fd11e
Ilya Karev <ilya.karev1000@gmail.com>
2016-05-11 12:33:43 +0700

    client info started

--- imports/ui/crm/clients/clients.component.js
+++ imports/ui/crm/clients/clients.component.js
@@ -6,16 +6,22 @@ import angularMeteor from 'angular-meteor';
 import {name as addClient} from './add-client/add-client.component';
 import {name as listMyClients} from './list-my-clients/list-my-clients.component';
 import {name as listHotClients} from './list-hot-clients/list-hot-clients.component';
+import {name as currentClient} from '/imports/ui/shared/client-info/client-info.component';
 import routes from './clients.routes';
 
 import './clients.view.html';
 
 class Clients {
   /* @ngInject */
-  // constructor($scope, $reactive) {
-  //   $reactive(this).attach($scope);
-  // }
-
+  constructor($scope, $reactive) {
+    $reactive(this).attach($scope);
+    
+    $scope.$on('sendingCurrentClient', function (event, data) {
+        console.log(data); // Данные, которые нам прислали
+    });
+    
+  }
+  
 }
 
 const moduleName = 'clients';
@@ -25,7 +31,8 @@ export default angular.module(moduleName, [
   angularMeteor,
   listMyClients,
   listHotClients,
-  addClient
+  addClient,
+  currentClient
 ]).component(moduleName, {
     templateUrl: 'imports/ui/crm/clients/clients.view.html',
     bindings: {},
