6273bfadf2a45c4aa7517b86d6d7a8612a4cb784
Pankrashin Danil <danpan@yandex.ru>
2016-05-14 23:31:21 +0300

    email

--- server/main.js
+++ server/main.js
@@ -1,7 +1,8 @@
 
-import '/imports/api/agents';
+import '/imports/api/helpers/send-email.method';
 import '/imports/api/locations';
 import '/imports/api/startup';
 import '/imports/api/realty';
+import '/imports/api/clients';
 import '/imports/api/clients';
 import '/imports/api/client-filter-query';
