a15a61dd247461e22bb2d9d3cf7f8dca9dabfdeb
Pankrashin Danil <danpan@yandex.ru>
2016-05-18 14:09:10 +0300

    select Добавить 0.1.7

--- imports/ui/layout/layout.component.js
+++ imports/ui/layout/layout.component.js
@@ -4,22 +4,24 @@
 import angular from 'angular';
 import {Meteor} from 'meteor/meteor';
 import angularMeteor from 'angular-meteor';
-import { Accounts } from 'meteor/accounts-base';
+import {Accounts} from 'meteor/accounts-base';
 
 import './layout.view.html';
 
 class Layout {
   /* @ngInject */
-  constructor($scope, $reactive, $mdSidenav) {
+  constructor($scope, $reactive, $mdSidenav, $state, $timeout) {
     $reactive(this).attach($scope);
+    this.$state = $state;
+    this.$timeout = $timeout;
     this.$mdSidenav = $mdSidenav;
     this.sideNavItems = [
       // {name: 'Добавить клиента', uisref: 'crm.clients.add'},
       // {name: 'Объекты', uisref: 'crm.realty.list.new'},
-      {name: 'Мои объекты', uisref: 'crm.realty.list.my'},
+      {name: 'Объекты', uisref: 'crm.realty.list.my'},
       // {name: 'Новые объекты', uisref: 'crm.realty.list.new'},
       // {name: 'Архив объекты', uisref: 'crm.realty.list.my'+'({status: \'archive\'})'},
-      {name: 'Мои Клиенты', uisref: 'crm.clients.list.my'+'({status: \'realtor\'})'},
+      {name: 'Клиенты', uisref: 'crm.clients.list.my' + '({status: \'realtor\'})'},
       // {name: 'Горячие Клиенты', uisref: 'crm.clients.list.my'+'({status: \'hot\'})'},
       // {name:'Дела', uisref:'realty'},
       // {name:'Отчет', uisref:'realty'},
@@ -28,8 +30,9 @@ class Layout {
       // {name:'Настройки', uisref:'realty'},
       // {name: 'Входящие Колл-центр', uisref: 'call-center.incoming'},
       // {name: 'Исходящие Колл-центр', uisref: 'call-center.outgoing'},
-      {name:'Модератор', uisref:'call-center.moderator'}
+      {name: 'Модератор', uisref: 'call-center.moderator'}
     ];
+    this.select = 0;
     this.autorun(function () {
       let user = Meteor.user();
       if (user) {
@@ -37,6 +40,18 @@ class Layout {
         this.user = user;
       }
     });
+  }
+
+  redirect(state) {
+    this.$state.go(state);
+    // this.$timeout(()=> {
+    // }, 100);
+
+  }
+
+  selectText() {
+    this.select = 0;
+    return 'Добавить';
   }
 
   toggleList() {
@@ -54,11 +69,11 @@ const moduleName = 'layout';
 export default angular.module(moduleName, [
   angularMeteor
 ]).component(moduleName, {
-    templateUrl: 'imports/ui/layout/layout.view.html',
-    bindings: {},
-    controllerAs: moduleName,
-    controller: Layout
-  })
+  templateUrl: 'imports/ui/layout/layout.view.html',
+  bindings: {},
+  controllerAs: moduleName,
+  controller: Layout
+})
   .config(mdThemeConfig)
   .config(mdIconConfig);
 
@@ -79,38 +94,37 @@ function mdIconConfig($mdIconProvider) {
     .icon('conditions_phone', 'svg/phone.svg', 24)
     .icon('conditions_animal', 'svg/animal.svg', 24)
     .icon('conditions_wifi', 'svg/wifi.svg', 24);
-    
+
   /*  
-  $mdIconProvider
-    .icon('cond_desktop', 'svg/cond_desktop.svg', 24)
-    .icon('cond_event_seat', 'svg/cond_event_seat.svg', 24)
-    .icon('cond_kitchen', 'svg/cond_kitchen.svg', 24)
-    .icon('cond_laundry', 'svg/cond_laundry.svg', 24)
-    .icon('cond_pets', 'svg/cond_pets.svg', 24)
-    .icon('cond_restaurant', 'svg/cond_restaurant.svg', 24)
-    .icon('cond_wifi', 'svg/cond_wifi.svg', 24);*/
-    
-    
-    
+   $mdIconProvider
+   .icon('cond_desktop', 'svg/cond_desktop.svg', 24)
+   .icon('cond_event_seat', 'svg/cond_event_seat.svg', 24)
+   .icon('cond_kitchen', 'svg/cond_kitchen.svg', 24)
+   .icon('cond_laundry', 'svg/cond_laundry.svg', 24)
+   .icon('cond_pets', 'svg/cond_pets.svg', 24)
+   .icon('cond_restaurant', 'svg/cond_restaurant.svg', 24)
+   .icon('cond_wifi', 'svg/cond_wifi.svg', 24);*/
+
+
   /*const planetPackageBower = '/packages/planettraining_material-design-icons/bower_components/';
 
-  const iconPath = planetPackageBower + 'material-design-icons/sprites/svg-sprite/';
+   const iconPath = planetPackageBower + 'material-design-icons/sprites/svg-sprite/';
 
-  $mdIconProvider
-    .iconSet('social',
-      iconPath + 'svg-sprite-social.svg')
-    .iconSet('action',
-      iconPath + 'svg-sprite-action.svg')
-    .iconSet('communication',
-      iconPath + 'svg-sprite-communication.svg')
-    .iconSet('content',
-      iconPath + 'svg-sprite-content.svg')
-    .iconSet('toggle',
-      iconPath + 'svg-sprite-toggle.svg')
-    .iconSet('navigation',
-      iconPath + 'svg-sprite-navigation.svg')
-    .iconSet('image',
-      iconPath + 'svg-sprite-image.svg');*/
+   $mdIconProvider
+   .iconSet('social',
+   iconPath + 'svg-sprite-social.svg')
+   .iconSet('action',
+   iconPath + 'svg-sprite-action.svg')
+   .iconSet('communication',
+   iconPath + 'svg-sprite-communication.svg')
+   .iconSet('content',
+   iconPath + 'svg-sprite-content.svg')
+   .iconSet('toggle',
+   iconPath + 'svg-sprite-toggle.svg')
+   .iconSet('navigation',
+   iconPath + 'svg-sprite-navigation.svg')
+   .iconSet('image',
+   iconPath + 'svg-sprite-image.svg');*/
 }
 
 function mdThemeConfig($mdThemingProvider) {
