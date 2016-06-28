b2fa10393c7ac2da5129209248068a303d51194c
Pankrashin Danil <danpan@yandex.ru>
2016-06-28 18:39:20 +0300

    оператор статистика

--- imports/api/operators/methods/operator-get-stat.method.js
+++ imports/api/operators/methods/operator-get-stat.method.js
@@ -8,52 +8,80 @@ Meteor.methods({
 });
 
 export function operatorGetStat() {
-    
-  if (Meteor.isServer && Meteor.userId()) {
-    let nullMatch = {};  
 
-    let opers = Operators.aggregate([{$match : nullMatch},
+  if (Meteor.isServer && Meteor.userId()) {
+    let nullMatch = {};
+
+    /*
+     db.operators.aggregate([{$group: {
+     _id: { type: '$result.type' },
+     totalType: { $sum: 1 }
+     }},
      {
+     $group: {
+     _id: { operatorId: '$operatorId' }
+     }
+     }, {
+     $sort: { '_id.operatorId': 1 }
+     }])
+     */
+
+
+    let opers = Operators.aggregate([{
       $group: {
-        _id: { operatorId: "$operatorId" }
+        _id: {type: '$result.type', operatorId: '$operatorId'},
+        totalType: {$sum: 1}
       }
     }, {
-      $sort: { '_id.operatorId': 1 }
-    }]).map((u)=>{
-      return {id:u._id.operatorId};
-    })
-
-    for (let o in opers) {
-
-      var oper = Meteor.users.findOne({_id:opers[o].id});
-      console.log(oper);
-      if(oper){
-        opers[o].name = oper.profile.name + ' ' + oper.profile.surName;
-        opers[o].phone = oper.profile.phone;
-        opers[o].email = oper.emails[0].address;
+      $group: {
+        _id: {operatorId: '$_id.operatorId'},
+        totalCalls: {$sum: '$totalType'},
+        stats: {$push: {type: '$_id.type', count: '$totalType'}}
       }
+    }, {
+      $sort: {'_id.operatorId': 1}
+    }]).map((operator)=> {
+      let newOperator = {};
+      operator.stats.forEach((stat)=> {
+        newOperator[stat.type] = stat.count;
+      });
+      newOperator.id = operator._id.operatorId;
+      newOperator.totalCalls = operator.totalCalls;
+      return newOperator;
+    });
+    console.log(opers);
 
-      let curMatch = {operatorId: opers[o].id};
-      opers[o].results = Operators.aggregate([{$match : curMatch},
-      {
-        $group: {
-          _id: { type: "$result.type" },
-          totalType: { $sum: 1 }
-        }
-      }, {
-        $sort: { 'result.type': 1 }
-      }]).map((u)=>{
-        if(u._id.type == 'objectsAgency') opers[o].objectsAgency = u.totalType;
-        if(u._id.type == 'objectsDelayed') opers[o].objectsDelayed = u.totalType;
-        if(u._id.type == 'objectsNotAvailable') opers[o].objectsNotAvailable = u.totalType;
-        if(u._id.type == 'objectsNoActual') opers[o].objectsNoActual = u.totalType;
-        if(u._id.type == 'objectsSkipped') opers[o].objectsSkipped = u.totalType;
-        if(u._id.type == 'objectsSaved') opers[o].objectsSaved = u.totalType;
-      })
-    }
+// for (let o in opers) {
+//
+//   var oper = Meteor.users.findOne({_id: opers[o].id});
+//   console.log(oper);
+//   if (oper) {
+//     opers[o].name = oper.profile.name + ' ' + oper.profile.surName;
+//     opers[o].phone = oper.profile.phone;
+//     opers[o].email = oper.emails[0].address;
+//   }
+//
+//   let curMatch = {operatorId: opers[o].id};
+//   opers[o].results = Operators.aggregate([{$match: curMatch},
+//     {
+//       $group: {
+//         _id: {type: '$result.type'},
+//         totalType: {$sum: 1}
+//       }
+//     }, {
+//       $sort: {'result.type': 1}
+//     }]).map((u)=> {
+//     if (u._id.type == 'objectsAgency') opers[o].objectsAgency = u.totalType;
+//     if (u._id.type == 'objectsDelayed') opers[o].objectsDelayed = u.totalType;
+//     if (u._id.type == 'objectsNotAvailable') opers[o].objectsNotAvailable = u.totalType;
+//     if (u._id.type == 'objectsNoActual') opers[o].objectsNoActual = u.totalType;
+//     if (u._id.type == 'objectsSkipped') opers[o].objectsSkipped = u.totalType;
+//     if (u._id.type == 'objectsSaved') opers[o].objectsSaved = u.totalType;
+//   })
+// }
 
     return opers;
   }
-  
+
 }
 
