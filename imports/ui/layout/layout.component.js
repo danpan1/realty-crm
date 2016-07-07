51126edc39eb35a6ea269d2b15a1688f3e7fb95c
Gadzhimari Kachaev <gadzhimari@gmail.com>
2016-07-07 14:57:20 +0300

    Hide temporary total amount deal in toolbar.

--- imports/ui/layout/layout.component.js
+++ imports/ui/layout/layout.component.js
@@ -30,7 +30,6 @@ class Layout {
       //{name:'Помощь', href:'https://vk.com/write19844032'},
       {name:'Помощь', uisref:'https://vk.com/write19844032'},
       {name:'150 за 6', uisref:'http://murigin.ru/intensiv/'},
-      // {name:'Выход', ngСlick:'layout.logout()', uisref:'auth.login'},
       // {name: 'Настройки', uisref:'realty'},
       // {name: 'Входящие Колл-центр', uisref: 'call-center.incoming'},
       // {name: 'Исходящие Колл-центр', uisref: 'call-center.outgoing'},
